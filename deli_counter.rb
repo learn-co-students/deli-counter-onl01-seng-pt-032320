307a52ed961123f8be74f22a51ed8f12ea8586dc
Wenhao Guo <gwh1742072653@gmail.com>
2020-03-30 19:38:31 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def line(array)
+  str = "The line is currently:"
+  num = 0
+  if array.size == 0 
+    puts "The line is currently empty."
+  else
+    while num < array.size
+      str << " " + (num + 1).to_s + ". " + array[num]
+      num += 1
+    end
+    puts str
+  end
+end
+
+def take_a_number(array,name)
+  array << name 
+  puts "Welcome, #{name}. You are number #{array.size} in line."
+end
+
+def now_serving(array)
+  if array.size == 0 
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}." 
+    array.shift
+  end
+end 
