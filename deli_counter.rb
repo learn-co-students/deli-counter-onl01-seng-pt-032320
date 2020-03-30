9d14c8e42444dcfb71737dc93b2c7f9ea08d69b7
Tanner Townsend <rsmcwow@gmail.com>
2020-03-30 01:05:32 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else 
+    message = "The line is currently:"
+    katz_deli.each_with_index do |name,index|
+      message += " #{index+1}. #{name}"
+    end
+    puts message
+  end
+end
+
+def take_a_number(katz_deli,person)
+  katz_deli.push(person)
+  puts "Welcome, #{person}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    person = katz_deli.shift
+    puts "Currently serving #{person}."
+  end
+end
