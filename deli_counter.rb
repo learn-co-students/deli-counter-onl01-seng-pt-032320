c0cc337e056c859af7c430324574871efbd649a5
Dina Kendle <dina.kendle@gmail.com>
2020-04-01 23:10:55 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,14 +5,36 @@ def line(katz_deli)
   
   if katz_deli.length == 0
      puts "The line is currently empty."
-      
   else
    katz_deli.each_with_index do |name, index|
       name_index << "#{index + 1}. #{name}"
+      
     end
     puts "The line is currently: #{name_index.join(" ")}"
    end
    name_index
 end   
 
+
+
+def take_a_number(katz_deli,name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+  
+  
+  
+def now_serving(katz_deli)
+ removed_names = Array.new
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else katz_deli.length > 0
+    katz_deli.each do |name| 
+     removed_names << "Currently serving #{name}." 
+    end
+       katz_deli.shift()
+
+  end
+end
+  
   
