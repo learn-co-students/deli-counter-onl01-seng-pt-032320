1a8baf68946a7f8aa7c39b0b45368ccc6cae3735
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-04-03 21:18:02 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli) 
+  phrase = "The line is currently:"
+  if katz_deli.length > 0 
+   katz_deli.each_with_index do |name, index| 
+     phrase += " #{index + 1}. #{name}" 
+    end 
+    puts phrase 
+  else 
+    puts "The line is currently empty."
+  end 
+end
+
+
+def take_a_number(katz_deli_line, name)
+  katz_deli_line = []
+  katz_deli_line << name
+  katz_deli_line.each do |names|
+  katz_deli_line.push(names)
+end
+katz_deli
+end 
