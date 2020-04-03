b7f4c96b6f1a439342405c112db243199400d9c7
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-03 22:48:11 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,5 @@
-# Write your code here.
+# Write your
+
 def line(new_deli)
 the_line = []
 
@@ -8,15 +9,22 @@ if new_deli.size == 0
 elsif new_deli.size > 0
   new_deli.each_with_index do |a_name, their_spot| 
     the_line << "#{their_spot + 1}. #{a_name}"
-puts "The line is currently: #{the_line}" 
 end
+puts "The line is currently: #{the_line.join(" ")}" 
 end 
-
+end
+ 
 def take_a_number(new_deli, next_p)
-  new_ticket = []
-  new_deli << next_p
-  new_deli.each_with_index do |x, y|
-  new_ticket << "Welcome, #{x}. You are number #{y + 1} in line."
-  puts new_ticket
+new_deli << next_p
+take_number_greeting = "Welcome, #{next_p}. You are number #{new_deli.index(next_p) + 1} in line."
+puts take_number_greeting
+end
+
+def now_serving(new_deli)
+if new_deli.length == 0
+  puts "There is nobody waiting to be served!"
+elsif new_deli.length > 0
+  puts "Currently serving #{new_deli.shift()}."
 end
 end
+
